fa453db5c4400cc735d62a71d944f47455f74e6c
shangskr <2729595419@qq.com>
2024-04-20 15:56:24 +0800

    Site updated: 2024-04-20 15:56:08

---
+++ js/shangskr.js
@@ -0,0 +1,15 @@
+var shangskr = {
+    applyFlink: function() {
+        var input = document.getElementsByClassName('el-textarea__inner')[0];
+        let evt = document.createEvent('HTMLEvents');
+        evt.initEvent('input', true, true);
+        input.value = '昵称（请勿包含博客等字样）：\n网站地址（要求博客地址，请勿提交个人主页）：\n头像图片url（请提供尽可能清晰的图片，我会上传到我自己的图床）：\n描述：\n';
+        input.dispatchEvent(evt);
+        var targetElement = document.getElementById("post-comment");
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        }
+        input.focus();
+        input.setSelectionRange(-1, -1);
+    },
+};
